0bf3dfe6aa34ec6adf3674befa768514587ed919
fragmetal <fragmetal88@gmail.com>
2024-11-07 09:50:36 +0800

    Test

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ const PORT = 3000;
 
 // Proxy request handler
 app.use('/', proxy('https://replit.com', {
-    proxyReqOptDecorator: (proxyReqOpts) => {
+    proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
         proxyReqOpts.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36';
+        proxyReqOpts.headers['Cookie'] = srcReq.headers['cookie'] || '';
         return proxyReqOpts;
-    }
+    },
+    userResHeaderDecorator: (headers) => {
+        headers['Cache-Control'] = 'public, max-age=3600';
+        return headers;
+    },
+    preserveHostHdr: true
 }));
 
 // Create an HTTP server with Express app
